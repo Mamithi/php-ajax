ec49ebe93767ded6b139b758fb5a22759ebc2597
Mamithi <19lawrence93@gmail.com>
2018-01-06 08:32:51 -0800

    Php-ajax first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+</head>
+<body>
+ 	<div class="container" style="margin-top: 30px;">
+ 		<div class="row">
+ 			<h2>Mysql Table Manager</h2>
+ 			<div class="col-md-8 col-offset-2">
+ 				<input type="button" class="btn btn-success" value="Add New" style="float: right;">
+ 				<br><br>
+ 				<table class="table table-hover table-bordered">
+ 					<thead>
+ 						<tr>
+ 							<td>Id</td>
+ 							<td>Country Name</td>
+ 							<td>Options</td>
+ 						</tr>
+ 					</thead>
+ 				</table>
+ 			</div>
+ 		</div>
+ 	</div>
+
+
+
+ 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+ 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+</body>
+</html>
