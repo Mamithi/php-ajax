6884417f6ac29be43bc8218c3646aa0acea63c4d
Mamithi <19lawrence93@gmail.com>
2018-01-06 15:37:49 -0800

    Add new data to the database

--- create.php
+++ create.php
@@ -1,3 +1,19 @@
 <?php
+ 	if(isset($_POST['key'])){
+ 		
+ 		$conn = new mysqli('localhost', 'root', '', 'php-ajax');
+ 		$name = $conn->real_escape_string($_POST['name']);
+ 		$shortDesc = $conn->real_escape_string($_POST['shortDesc']);
+ 		$longDesc = $conn->real_escape_string($_POST['longDesc']);
 
+ 		if ($_POST['key'] == 'addNew') {
+ 			$sql = $conn->query("SELECT id FROM country WHERE country_name = '$name'");
+ 			if($sql->num_rows > 0){
+ 				exit("Country with this name already exists");
+ 			}else{
+ 				$conn->query("INSERT INTO country (country_name, short_description, long_description) VALUES('$name', 'shortDesc', 'longDesc')");
+ 				exit("Country has been inseretd successfully");
+ 			}
+ 		}
+ 	}
 ?>
